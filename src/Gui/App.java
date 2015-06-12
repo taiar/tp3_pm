a203e7259ea73a0ab3f16e3fc6834ae6c1d2b73c
André Taiar <taiar@dcc.ufmg.br>
2015-06-12 12:21:08 -0300

    Novo constrututor parametrizado.

--- src/Gui/App.java
+++ src/Gui/App.java
@@ -8,6 +8,10 @@ public class App {
         this.appWindow = new MainWindow("TP3 PM | Gerenciador de Campeonatos esportivos", 800, 600);
     }
 
+    public App(String titulo, int largura, int altura) {
+        this.appWindow = new MainWindow(titulo, largura, altura);
+    }
+
     public void exec() {
         this.appWindow.showApp();
     }
